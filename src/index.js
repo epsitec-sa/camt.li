e53391019b03b391b67d172d78e8646168803cf7
Pierre Arnaud <arnaud@epsitec.ch>
2016-06-05 10:54:45 +0200

    First fully working implementation with a nice UI.

--- src/index.js
+++ src/index.js
@@ -64,22 +64,39 @@ function getDetailsSummary (xml) {
   const remittanceInformation = xml.match (/<RmtInf>(.+)<\/RmtInf>/);
   const debtorName = financialInstitution && financialInstitution[1].match (/<Nm>([a-zA-Z0-9_\-.:;+/ ]*)</);
   const reference = remittanceInformation && remittanceInformation[1].match (/<Ref>(.*)<\/Ref>/);
-  return `<li>
-<div>Montant: <strong>${amount[2]} ${amount[1]}</strong></div>
-<div>Mouvement: ${credit ? credit[1] : '-'}</div>
-<div>Débiteur: ${debtorName ? escapeXml (debtorName[1]) : '-'}</div>
-<div>Référence: ${reference ? escapeXml (reference[1]) : '-'}</div>
-<div>Frais: ${charges ? `${charges[2]} ${charges[1]}` : '-'}</div>
-</li>`;
+  return `
+<tr>
+  <td>Mouvement:</td>
+  <td>${credit ? credit[1] : '-'}</td>
+</tr>
+<tr>
+  <td>Débiteur:</td>
+  <td>${debtorName ? escapeXml (debtorName[1]) : '-'}</td>
+</tr>
+<tr>
+  <td>Référence:</td>
+  <td>${reference ? escapeXml (reference[1]) : '-'}</td>
+</tr>
+<tr>
+  <td>Frais:</td>
+  <td class="align-right">${charges ? `${charges[2]} ${charges[1]}` : '-'}</td>
+</tr>
+<tr>
+  <td>Montant:</td>
+  <td class="bold align-right">${amount[2]} ${amount[1]}</td>
+</tr>
+`;
 }
 
 function getEntrySummary (xml) {
-  const amount = xml.match (/<Amt Ccy="(...)">(\d+\.\d+)<\/Amt/);
+  const amount  = xml.match (/<Amt Ccy="(...)">(\d+\.\d+)<\/Amt/);
   const charges = xml.match (/<TtlChrgsAndTaxAmt Ccy="(...)">(\d+\.\d+)<\/TtlChrgsAndTaxAmt/);
-  const infos = xml.match (/<AddtlNtryInf>(.+)<\/AddtlNtryInf/);
+  const infos   = xml.match (/<AddtlNtryInf>(.+)<\/AddtlNtryInf/);
+  
   const bookingDate = getDate (xml, '<BookgDt><Dt>');
-  const valutaDate = getDate (xml, '<ValDt><Dt>');
-  let details = '<ul>';
+  const valutaDate  = getDate (xml, '<ValDt><Dt>');
+  
+  let details = '';
   let start = 0;
   while (true) {
     start = xml.indexOf ('<TxDtls>', start);
@@ -93,39 +110,78 @@ function getEntrySummary (xml) {
     }
     details += getDetailsSummary (xml.substring (start, end));
   }
-  details += '</ul>';
-  return `<li>
-<h2>${infos ? infos[1] : '-'}</h2>
-<div>Total: <strong>${amount[2]} ${amount[1]}</strong></div>
-<div>Total des frais: ${charges ? `${charges[2]} ${charges[1]}` : '-'}</div>
-<div>Date de comptabilisation: ${bookingDate}</div>
-<div>Date valeur: ${valutaDate}</div>
-${details}
-</li>`;
+  
+  const title = infos ? infos[1] : '-';
+  
+  let html = `
+<table cellpadding="0" cellspacing="0" class="transaction">
+  <caption style="text-align: left;">
+    <h3>${title}</h3>
+  </caption>
+  <tbody>
+    <tr>
+      <td>Total:</td>
+      <td class="bold align-right">${amount[2]} ${amount[1]}</td>
+    </tr>`;
+  if (charges) {
+    html += `
+    <tr>
+      <td>Total des frais:</td>
+      <td class="bold align-right">${charges[2]} ${charges[1]}</td>
+    </tr>`
+  };
+  html += `
+    <tr>
+      <td>Date de comptabilisation:</td>
+      <td class="align-right">${bookingDate}</td>
+    </tr>
+    <tr>
+      <td>Date valeur:</td>
+      <td class="align-right">${valutaDate}</td>
+    </tr>`;
+ html += details;
+ html += `
+  </tbody>
+</table>`;
+
+   return html;
 }
 
-function getBalanceSummary (xml) {
-  let output = '';
+/******************************************************************************/
+
+function getBalanceSummary (xml, output) {
   const cd = xml.match (/<Cd>(\w+)<\/Cd>/);
   const amount = xml.match (/<Amt Ccy="(...)">(\d+\.\d+)<\/Amt/);
   const date = getDate (xml, '<Dt>');
   if (cd) {
     switch (cd[1]) {
       case 'OPBD':
-        output += `<div>Solde d'ouverture: ${amount[2]} ${amount[1]} (${date})</div>`;
+        output.open = `
+<table cellpadding="0" cellspacing="0" class="solde_ouverture" style="font-size: 1.5em;">
+  <tr>
+    <td>Solde d'ouverture (${date})</td>
+    <td class="bold align-right">${amount[2]} ${amount[1]}</td>
+  </tr>
+</table>`;
         break;
       case 'CLBD':
-        output += `<div>Solde de clôture: ${amount[2]} ${amount[1]} (${date})</div>`;
+        output.close = `
+<table cellpadding="0" cellspacing="0" class="solde_fermeture" style="font-size: 1.5em;">
+<tr>
+  <td>Solde de clôture (${date})</td>
+  <td class="bold align-right">${amount[2]} ${amount[1]}</td>
+</tr>
+</table>`;
         break;
     }
   }
-  return output;
 }
 
-function getEntriesSummary (xml) {
-  let count = 0;
-  let output = '<ul>';
+function getEntriesSummaryNtry (xml, output) {
   let start = 0;
+  
+  output.entries = [];
+  
   while (true) {
     start = xml.indexOf ('<Ntry>', start);
     if (start < 0) {
@@ -137,10 +193,15 @@ function getEntriesSummary (xml) {
       break;
     }
     const entry = xml.substring (start, end);
-    output += getEntrySummary (entry);
-    count++;
+    const html  = getEntrySummary (entry);
+    if (html) {
+      output.entries.push (html);
+    }
   }
-  start = 0;
+}
+
+function getEntriesSummaryBal (xml, output) {
+  let start = 0;
   while (true) {
     start = xml.indexOf ('<Bal>', start);
     if (start < 0) {
@@ -148,14 +209,13 @@ function getEntriesSummary (xml) {
     }
     start += 5;
     let end = xml.indexOf ('</Bal>', start);
-    const balance = getBalanceSummary (xml.substring (start, end));
-    if (balance && balance.length > 0) {
-      output += balance;
-      count++;
-    }
+    getBalanceSummary (xml.substring (start, end), output);
   }
-  output += '</ul>';
-  return count && output;
+}
+
+function getEntriesSummary (xml, output) {
+  getEntriesSummaryNtry (xml, output);
+  getEntriesSummaryBal (xml, output);
 }
 
 function getCustomerAccount (xml) {
@@ -163,42 +223,52 @@ function getCustomerAccount (xml) {
   return result && `IBAN ${formatIBAN (result[1])}` || `-`;
 }
 
-function getXmlCamt53V4Report (title, xml) {
-  const transactions = getEntriesSummary (xml);
-  let output = `
-<h1>Fichier camt.053 (V4)</h1>
-<div>Fichier: ${escapeXml (title)}</div>
-<div>Date de création: ${getCreationDateTime (xml)}</div>
-<div>Compte client: ${getCustomerAccount (xml)}</div>`;
-  if (transactions) {
-    output += `
-<div>Transactions: ${transactions}</div>`;
-  }
-  return output;
-}
+/******************************************************************************/
 
-function getXmlCamt54V4Report (title, xml) {
-  const transactions = getEntriesSummary (xml);
-  let output = `
-<h1>Fichier camt.054 (V4)</h1>
-<div>Fichier: ${escapeXml (title)}</div>
-<div>Date de création: ${getCreationDateTime (xml)}</div>
-<div>Compte client: ${getCustomerAccount (xml)}</div>`;
-  if (transactions) {
-    output += `
-<div>Transactions: ${transactions}</div>`;
+function getXmlCamtReport (fileName, title, xml) {
+  let output = {};
+  let transactions = '';
+  
+  getEntriesSummary (xml, output);
+  
+  if (output.entries.length) {
+    transactions += `<h2 class="">Transactions</h2>`;
+    output.entries.forEach (entry => transactions += entry + '\n');
   }
-  return output;
+  
+  return `
+<table cellpadding="0" cellspacing="0">
+  <caption>
+    <h1>${title}</h1>
+  </caption>
+  <tbody>
+    <tr>
+      <td>Fichier:</td>
+      <td>${escapeXml (fileName)}</td>
+    </tr>
+    <tr>
+      <td>Date de création:</td>
+      <td>${getCreationDateTime (xml)}</td>
+    </tr>
+    <tr>
+      <td>Compte client:</td>
+      <td>${getCustomerAccount (xml)}</td>
+    </tr>
+  </tbody>
+</table>
+${output.open || ''}
+${transactions}
+${output.close || ''}`;
 }
 
 function getXmlReport (title, xml) {
   if (xml.indexOf (`<Document xmlns="${xsdCamt53V4}" `) > 0) {
-    return getXmlCamt53V4Report (title, xml);
+    return getXmlCamtReport (title, 'Fichier camt.053 (V4)', xml);
   }
   if (xml.indexOf (`<Document xmlns="${xsdCamt54V4}" `) > 0) {
-    return getXmlCamt54V4Report (title, xml);
+    return getXmlCamtReport (title, 'Fichier camt.054 (V4)', xml);
   }
-  return 'Ce fichier possède un format non reconnu.';
+  return `<h1 class="error">Ce fichier possède un format non reconnu.</h1>`;
 }
 
 /******************************************************************************/
@@ -208,22 +278,26 @@ function handleFileSelect (evt) {
   evt.preventDefault ();
 
   const files = evt.dataTransfer.files;
-  const output = document.getElementById ('list');
+  const output = document.getElementById ('output');
 
   while (output.firstChild) {
     output.removeChild (output.firstChild);
   }
 
   for (var i = 0; i < files.length; i++) {
-    const xml = files[i];
-    const li = document.createElement ('li');
-    output.insertBefore (li, null);
-    const reader = new FileReader ();
+    const xml     = files[i];
+    const article = document.createElement ('article');
+    const reader  = new FileReader ();
     reader.onload = e => {
-        li.innerHTML = `<div>${getXmlReport (xml.name, e.target.result)}</div>`;
+        article.innerHTML = getXmlReport (xml.name, e.target.result);
       };
     reader.readAsText (xml);
+    output.insertBefore (article, null);
+    
+    $('html, body').animate ({scrollTop: 650}, 1000);
   }
+  
+  output.style.display = "block";
 }
 
 function handleDragOver (evt) {
